1688e151ec36a1e5619a3a1eb55b5b8237bc396b
suchov <artem.suchov@gmail.com>
2019-04-29 22:54:07 +0300

    quick fix passing the empty cars props to CardList to fix tests

--- src/CardList.test.js
+++ src/CardList.test.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import CardList from './components/CardList';
+import React from "react";
+import ReactDOM from "react-dom";
+import CardList from "./components/CardList";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<CardList />, div);
+it("renders without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<CardList cars={[]} />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
