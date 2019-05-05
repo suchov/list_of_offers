55d646b59da46eb7096eb114fada90ad294c934b
suchov <artem.suchov@gmail.com>
2019-05-05 08:20:25 +0300

    add some structure, add to ErrorBoundary positive and negative tests

--- src/CardList.test.js
+++ src/CardList.test.js
@@ -1,22 +1,18 @@
 import React from "react";
 import Enzyme, { shallow } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
-
 import { findByTestAttr, checkProps } from "../test/testUtils";
 import CardList from "./components/CardList";
 
 Enzyme.configure({ adapter: new Adapter() });
 /**
- * Factory function to create a ShallowWrapper for the App component
+ * Factory function to create a ShallowWrapper for the CardList component
  * @function setup
  * @param {object} props - Component props specific for this setup
- * @param {any} state - Initial state for setup
  * @returns {ShallowWrapper}
  */
-const setup = (props = {}, state = null) => {
-  const wrapper = shallow(<CardList {...props} />);
-  if (state) wrapper.setState(state);
-  return wrapper;
+const setup = (props = {}) => {
+  return shallow(<CardList {...props} />);
 };
 
 test("renders without errors", () => {
